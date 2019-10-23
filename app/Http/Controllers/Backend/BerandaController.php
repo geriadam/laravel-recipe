0c3b47ecec11a1b3db9de9705258b7321f5845bc
DESKTOP-UPQ61CI\geria <geriadam1@gmail.com>
2019-10-23 10:00:21 +0700

    count list category, ingrendients, recipe to dashboard

--- app/Http/Controllers/Backend/BerandaController.php
+++ app/Http/Controllers/Backend/BerandaController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Ingrendient;
+use App\Models\Recipe;
 use Auth;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class BerandaController extends Controller
 {
     public function index()
     {
-        return view('backend.beranda.home');
+        $category_count     = Category::count();
+        $ingrendients_count = Ingrendient::count();
+        $recipe_count       = Recipe::count();
+
+        return view('backend.beranda.home', compact('category_count', 'ingrendients_count', 'recipe_count'));
     }
 }
--- resources/views/backend/beranda/home.blade.php
+++ resources/views/backend/beranda/home.blade.php
@@ -2,11 +2,41 @@
 
 @section('content')
 <div class="row">
-	<div class="col-auto">
-		<img src="{{ asset('img/avatar9.jpg') }}" alt="user" width="200px" height="200px">
-	</div>
-	<div class="col-auto">
-		
-	</div>
+	<div class="col-lg-4 col-sm-6">
+        <div class="card card-sm">
+            <div class="card-body">
+                <span class="d-block font-11 font-weight-500 text-dark text-uppercase mb-10">Category</span>
+                <div class="d-flex align-items-center justify-content-between position-relative">
+                    <div>
+                        <span class="d-block display-5 font-weight-400 text-dark">{{ $category_count }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-6">
+        <div class="card card-sm">
+            <div class="card-body">
+                <span class="d-block font-11 font-weight-500 text-dark text-uppercase mb-10">Ingrendients</span>
+                <div class="d-flex align-items-center justify-content-between position-relative">
+                    <div>
+                        <span class="d-block display-5 font-weight-400 text-dark">{{ $ingrendients_count }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-6">
+        <div class="card card-sm">
+            <div class="card-body">
+                <span class="d-block font-11 font-weight-500 text-dark text-uppercase mb-10">Recipe</span>
+                <div class="d-flex align-items-center justify-content-between position-relative">
+                    <div>
+                        <span class="d-block display-5 font-weight-400 text-dark">{{ $recipe_count }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
